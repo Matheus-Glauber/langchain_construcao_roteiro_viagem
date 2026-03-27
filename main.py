6ed7dceb55a3aad91f699373cdfde18d8a324ce1
Matheus Glauber <glauber.jordao1995@gmail.com>
2026-03-27 17:48:07 -0300

    Mudando o formato de saida com JsonOutputParser e logando informações de debug.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.globals import set_debug
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
+
+set_debug(True)
 
 load_dotenv()
 
@@ -10,11 +14,19 @@ api_key = os.getenv("GEMINI_API_KEY")
 
 atividade = "parques temáticos"
 
+class Destino(BaseModel):
+    cidade: str = Field(..., description="A cidade sugerida para visitar")
+    motivo: str = Field(..., description="O motivo pelo qual a cidade é recomendada para visitar")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 template_cidade = PromptTemplate(
     template=""""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
     input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()},
 )
 
 modelo = ChatGoogleGenerativeAI(
@@ -22,7 +34,7 @@ modelo = ChatGoogleGenerativeAI(
     google_api_key=api_key,
 )
 
-chain = template_cidade | modelo | StrOutputParser()
+chain = template_cidade | modelo | parseador
 
 response = chain.invoke({
     "interesse": atividade,
