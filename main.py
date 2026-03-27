0284969c993ae1070d014ce34330798e349d891f
Matheus Glauber <glauber.jordao1995@gmail.com>
2026-03-27 18:38:01 -0300

    Criando sequencia de cadeias com LangChain

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from langchain_core.globals import set_debug
+from langchain_core.runnables import RunnablePassthrough
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 
-set_debug(True)
+set_debug(False)
 
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 
-atividade = "parques temáticos"
+atividade = "Pontos turisticos no Chile"
 
 class Destino(BaseModel):
     cidade: str = Field(..., description="A cidade sugerida para visitar")
     motivo: str = Field(..., description="O motivo pelo qual a cidade é recomendada para visitar")
 
-parseador = JsonOutputParser(pydantic_object=Destino)
+class Restaurantes(BaseModel):
+    cidade: str = Field(..., description="A cidade sugerida para visitar")
+    restaurantes:str = Field(..., description="Uma lista de restaurantes recomendados na cidade sugerida")
+    motivo: str = Field(..., description="O motivo pelo esses restaurantes são recomendados")
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes)
 
 template_cidade = PromptTemplate(
     template=""""
@@ -26,7 +33,19 @@ template_cidade = PromptTemplate(
     {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida": parseador.get_format_instructions()},
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()},
+)
+
+template_restaurantes = PromptTemplate(
+    template=""""
+    Sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables={"formato_de_saida": parseador_restaurantes.get_format_instructions()},
+)
+
+template_cultural = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}"
 )
 
 modelo = ChatGoogleGenerativeAI(
@@ -34,10 +53,23 @@ modelo = ChatGoogleGenerativeAI(
     google_api_key=api_key,
 )
 
-chain = template_cidade | modelo | parseador
+chain_1 = template_cidade | modelo | parseador_destino
+chain_2 = template_restaurantes | modelo | parseador_restaurantes
+chain_3 = template_cultural | modelo | StrOutputParser()
+
+chain = chain_1 | { 
+    "destino": RunnablePassthrough(),
+    "restaurantes": chain_2, 
+    "cultura": chain_3
+}
 
 response = chain.invoke({
     "interesse": atividade,
 })
 
-print(response)
+print("=== Destino ===")
+print(response["destino"])
+print("\n=== Restaurantes ===")
+print(response["restaurantes"])
+print("\n=== Cultural ===")
+print(response["cultura"])
